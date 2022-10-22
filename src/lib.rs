71e8dffa0509b5dc701a8dd292667daca0b26956
Marcus Dunn <marcus.s.dunn@gmail.com>
2022-10-21 18:38:46 -0700

    rustfmt

--- src/lib.rs
+++ src/lib.rs
@@ -1,7 +1,12 @@
 //! Quality Qualitative Coding is an application for doing qualitative coding for interviews between
-//! two people where one person is the interviewer and the second the is interviewer who's responses 
+//! two people where one person is the interviewer and the second the is interviewer who's responses
 //! we are interested in coding  
 
-#![warn(clippy::all, missing_docs, missing_debug_implementations, missing_copy_implementations)]
+#![warn(
+    clippy::all,
+    missing_docs,
+    missing_debug_implementations,
+    missing_copy_implementations
+)]
 pub use app::QualityQualitativeCoding;
 mod app;
